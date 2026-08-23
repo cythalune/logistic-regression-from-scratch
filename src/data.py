5efafecbca3f6cdc109d0ff48efdbeb70eb1b8d5
cythalune <cythlune@gmail.com>
2026-08-23 21:48:04 +0500

    data stadardization

--- src/data.py
+++ src/data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np 
 
 def load_data():
     df = pd.read_csv(f'data/data.csv')
@@ -12,10 +13,36 @@ def load_data():
 
     #pandas dataframe -> numpy arrays
     X = train_df.iloc[:, 2:4].to_numpy()  
-    Y = train_df.iloc[:, 1].to_numpy() 
+    Y = train_df.iloc[:, 1].map({
+    "M": 1,
+    "B": 0 
+    }).to_numpy()
 
     X_test = test_df.iloc[:, 2:4].to_numpy()
-    Y_test = test_df.iloc[:, 1].to_numpy()
+    Y_test = test_df.iloc[:, 1].map({
+    "M": 1,
+    "B": 0 
+    }).to_numpy()
 
-    return X, Y, X_test, Y_test
+    X_standardized, X_test_standardized = standardize(X, X_test)
 
+    return X_standardized, Y, X_test_standardized, Y_test
+
+def standardize(X, X_test):
+    """
+    Standardize the features in the training and testing sets.
+
+    Parameters:
+        X (np.ndarray): Training features.
+        X_test (np.ndarray): Testing features.
+
+    Returns:
+        tuple: Standardized training and testing features.
+    """
+    mean = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
+
+    X_standardized = (X - mean) / (std + 1e-8)
+    X_test_standardized = (X_test - mean) / (std + 1e-8)
+
+    return X_standardized, X_test_standardized
